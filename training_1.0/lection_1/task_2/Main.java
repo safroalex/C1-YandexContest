83bdbd0b15a8254ad14e97547a0831e354534898
safroalex <alexsafr.a@ya.ru>
2023-05-22 16:11:35 +0300

    add task_2

--- training_1.0/lection_1/task_2/Main.java
+++ training_1.0/lection_1/task_2/Main.java
@@ -7,11 +7,11 @@ public class Main {
     public static final String YES = "YES";
     public static final String NO = "NO";
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            int a = scanner.nextInt(), b = scanner.nextInt(), c = scanner.nextInt();
 
-        int a = scanner.nextInt(), b = scanner.nextInt(), c = scanner.nextInt();
-
-        String result = (a < b + c && b < a + c && c < a + b) ? YES : NO;
-        System.out.println(result);
+            String result = (a < b + c && b < a + c && c < a + b) ? YES : NO;
+            System.out.println(result);
+        }
     }
 }
