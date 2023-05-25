c28830ef73e354a2f57ced845872c17b3680604a
safroalex <alexsafr.a@ya.ru>
2023-05-25 12:08:03 +0300

    fix task_3

--- training_1.0/lection_1/task_3/Main.java
+++ training_1.0/lection_1/task_3/Main.java
@@ -3,30 +3,35 @@ package task_3;
 import java.util.Scanner;
 
 public class Main {
+
+    private static final String CODE_NUMBER = "7";
+    private static final String CODE_AND_REGION = "7495";
+    private static final int PHONE_BOOK_SIZE = 3;
+
+    public static String formatNumber(String input) {
+        if (input.length() == 11 && input.charAt(0) == '8') {
+            input = "7" + input.substring(1);
+        } else if (input.length() == 10) {
+            input = CODE_NUMBER + input;
+        } else if (input.length() == 7) {
+            input = CODE_AND_REGION + input;
+        }
+        return input;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String newNumber = scanner.nextLine().replaceAll("[^0-9]", "");
-        String[] phoneBook = new String[3];
-        for (int i = 0; i < 3; i++) {
-            phoneBook[i] = scanner.nextLine().replaceAll("[^0-9]", "");
-            if (phoneBook[i].length() == 7) {
-                phoneBook[i] = "495" + phoneBook[i];
-            } else if (phoneBook[i].charAt(0) == '8') {
-                phoneBook[i] = "7" + phoneBook[i].substring(1);
-            } else if (phoneBook[i].charAt(0) == '7') {
-                phoneBook[i] = phoneBook[i].replaceFirst("7", "8");
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            String newNumber = formatNumber(scanner.nextLine().replaceAll("[^0-9]", ""));
+            String[] phoneBook = new String[PHONE_BOOK_SIZE];
+
+            for (int i = 0; i < phoneBook.length; i++) {
+                phoneBook[i] = formatNumber(scanner.nextLine().replaceAll("[^0-9]", ""));
             }
-        }
-        for (String number : phoneBook) {
-            if (number.equals(newNumber)) {
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+
+            for (String phone : phoneBook) {
+                System.out.println(newNumber.equals(phone) ? "YES" : "NO");
             }
-        }
-        for (int i = 0; i < phoneBook.length; i++) {
-            System.out.println(phoneBook[i]);
         }
     }
 }
-
