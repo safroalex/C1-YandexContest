6ed85dadd20e2821ecd1ecb0f29e446b27f023d5
safroalex <alexsafr.a@ya.ru>
2023-06-11 22:36:25 +0300

    add second solution

--- training_openLectory_mobileIOS/firstHmwrk/task1/main.swift
+++ training_openLectory_mobileIOS/firstHmwrk/task1/main.swift
@@ -1,13 +1,23 @@
-var countSteps = 1;
+/*
+ math soulution;
+ we'll use S=k*(k+1)/2 arithmetic formula
+ where S = amout of blocks, k = steps
+ n >= k*(k+1)/2
+ 2n >= k*(k+1)
+ 0 >= k*(k+1) - 2n
+ k^2 + k - 2n
+ D = 1 + 8n
+ k = -1 + sqrt(D) / 2
+ */
+import Foundation
 
-if let input = readLine(), var blocks = Int(input) {
-    while blocks > 0 {
-        blocks -= countSteps
-        if blocks - countSteps <= 0 {
-            break
-        }
-        countSteps += 1
-    }
+func solv(n: Int) -> Int {
+    let D = 1 + 8*n
+    let k = (-1 + sqrt(Double(D)))/2
+    return Int(k)
 }
-print(countSteps)
+
+if let input = readLine(), var n = Int(input) {
+    print(solv(n: n))
+}
 
